87d6512da1a5f20c673fd77a7d6e845b12c1e3bb
David Lu <david.scowluga@gmail.com>
2018-01-24 12:37:33 -0500

    Solved Neanderthal Numbers

---
+++ AAA/src/y2016_r3_p3/Main.java
@@ -0,0 +1,305 @@
+package y2016_r3_p3;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/* CamelCase
+
+dictionary mapping first letter?
+or maybe some sort of tree (probably don't need that)
+
+
+
+*/
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FastReader reader = new FastReader();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // > Initializing dictionary
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < alphabet.length(); i++) {
+            map.put(alphabet.substring(i, i+1), new ArrayList<>());
+        }
+
+        // > Inputting dictionary
+        int dn = reader.nextInt();
+        for (int i = 0; i < dn; i++) {
+            String s = reader.readLine();
+            map.get(s.substring(0, 1)).add(s);
+        }
+
+        // > Solve
+        for (int t = 0; t < 10; t++) {
+            String s = reader.readLine();
+            int[] dp = new int[s.length() + 2001];
+            dp[0] = 1;
+
+            for (int i = 0; i < s.length(); i++) { // each letter
+                if (dp[i] != 0) {
+                    List<String> words = map.get(s.substring(0, 1));
+                    for (String w : words) { // each possible word
+                        if (s.length() - i > w.length()) {
+                            if (w.equals(s.substring(i, i + w.length()))) {
+                                if (dp[i + w.length()] == 0) dp[i + w.length()] = dp[i];
+                                else dp[i + w.length()] = Math.min(dp[i + w.length()], dp[i]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println(dp[s.length()]);
+        }
+    }
+
+
+    public static class FastReader {
+
+        private final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[6400]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public List<Integer> readLineAsIntegers() throws IOException {
+//        int[] ret = new int[1024];
+            List<Integer> ret = new ArrayList<>();
+            int idx = 0;
+            byte c = read();
+            while (c != -1) {
+                if (c == '\n' || c == '\r')
+                    break;
+
+                // next integer
+                int i = 0;
+                while (c <= ' ') {
+                    c = read();
+                }
+                boolean negative = (c == '-');
+                if (negative) {
+                    c = read();
+                }
+
+                do {
+                    i = i * 10 + (c - '0');
+                    c = read();
+                } while (c >= '0' && c <= '9');
+//            ret[idx++] = (negative) ? -i : i;
+                ret.add((negative) ? -i : i);
+            }
+            return ret;
+        }
+
+        public List<Long> readLineAsLongs() throws IOException {
+            List<Long> ret = new ArrayList<>();
+            int idx = 0;
+            byte c = read();
+            while (c != -1) {
+                if (c == '\n' || c == '\r')
+                    break;
+
+                // next integer
+                long i = 0;
+                while (c <= ' ') {
+                    c = read();
+                }
+                boolean negative = (c == '-');
+                if (negative) {
+                    c = read();
+                }
+
+                do {
+                    i = i * 10 + (c - '0');
+                    c = read();
+                } while (c >= '0' && c <= '9');
+                ret.add((negative) ? -i : i);
+            }
+            return ret;
+        }
+
+        public List<Double> readLineAsDoubles() throws IOException {
+            List<Double> ret = new ArrayList<>();
+            int idx = 0;
+            byte c = read();
+            while (c != -1) {
+                if (c == '\n' || c == '\r')
+                    break;
+
+                // next integer
+                double d = 0, div = 1;
+                while (c <= ' ') {
+                    c = read();
+                }
+                boolean negative = (c == '-');
+                if (negative) {
+                    c = read();
+                }
+
+                do {
+                    d = d * 10 + (c - '0');
+                    c = read();
+                } while (c >= '0' && c <= '9');
+
+                if (c == '.') {
+                    while ((c = read()) >= '0' && c <= '9') {
+                        d += (c - '0') / (div *= 10);
+                    }
+                }
+                ret.add((negative) ? -d : d);
+            }
+            return ret;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            return (neg) ? -ret : ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            return (neg) ? -ret : ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            return (neg) ? -ret : ret;
+        }
+
+        public String nextString() throws IOException {
+            byte[] ret = new byte[1024];
+            int idx = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            do {
+                ret[idx++] = c;
+                c = read();
+            } while (c != -1 && c != ' ' && c != '\n' && c != '\r');
+            return new String(ret, 0, idx);
+
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                // fill buffer
+                bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+                if (bytesRead == -1) {
+                    buffer[0] = -1;
+                }
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public int[] readLineAsIntArray(int n) throws IOException {
+            int[] ret = new int[n];
+//            int ret = new ArrayList<>();
+            int idx = 0;
+            byte c = read();
+            while (c != -1) {
+                if (c == '\n' || c == '\r')
+                    break;
+
+                // next integer
+                int i = 0;
+                while (c <= ' ') {
+                    c = read();
+                }
+                boolean negative = (c == '-');
+                if (negative) {
+                    c = read();
+                }
+
+                do {
+                    i = i * 10 + (c - '0');
+                    c = read();
+                } while (c >= '0' && c <= '9');
+
+                ret[idx++] = (negative) ? -i : i;
+                if (idx >= n) {
+                    break;
+                }
+
+//                ret.add((negative) ? -i : i);
+            }
+            return ret;
+        }
+
+        public void close() throws IOException {
+            if (din != null) {
+                din.close();
+            }
+        }
+
+    }
+}
